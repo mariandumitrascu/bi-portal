c33d97f1af678439d227d1ee2acc6cd1847d1690
Marian Dumitrascu <md@dumitrascu.net>
2021-01-07 08:15:28 -0600

    ch

--- mainsite/biportal/admin.py
+++ mainsite/biportal/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-
 from .models import Presentation, Bipage, Snippet
 
 # Register your models here.
 
 
-class BipageInline(admin.StackedInline):
+# class BipageInline(admin.StackedInline):
+class BipageInline(admin.TabularInline):
     model = Bipage
+    show_change_link = True
+    fields = ['name', 'last_updated']
+    readonly_fields = ['last_updated']
+    list_display = ['name', 'last_updated']
+
+    # prepopulated_fields = {"name": ("",)}
     extra = 3
 
 @admin.register(Presentation)
@@ -14,22 +20,23 @@ class PresentationAdmin(admin.ModelAdmin):
 
     # fields = ['name', 'description', 'active', 'created_by', 'tags']
 
+    readonly_fields = ['created_at']
+
     fieldsets = [
         (
             'Basic Information',
             {
-                'fields': ['name', 'description','created_by', 'active']
+                'fields': ['name', 'description', 'active']
             }
         ),
 
         (
             'Metadata',
             {
-                'fields': ['tags']
+                'fields': ['created_by', 'created_at', 'tags']
             }
         ),
     ]
-
 
     search_fields = ['name', 'description',]
 
@@ -41,7 +48,50 @@ class PresentationAdmin(admin.ModelAdmin):
 
     inlines = [BipageInline]
 
+    save_as = True
+
+    list_per_page = 5
+
+
+###################################################################################################
+###################################################################################################
+###################################################################################################
+@admin.register(Snippet)
+class SnippetAdmin(admin.ModelAdmin):
+    readonly_fields = ['created_at']
+
+    fieldsets = [
+        (
+            'Basic Information',
+            {
+                'fields': ['name', 'embedded']
+            }
+        ),
+        (
+            'Snippet Image',
+            {
+                'fields': ['image_rendered', 'image_cropped']
+            }
+        ),
+        (
+            'Position and Dimensions',
+            {
+                'fields': [('x', 'y'), ('w', 'h')],
+                # 'description': 'sfdsfd dsf sdf ds fds  fds  f'
+            }
+        ),
+
+        (
+            'Metadata',
+            {
+                'fields': ['created_by', 'created_at', 'tags']
+            }
+        ),
+    ]
+
+    list_display = ['name', 'created_by', 'created_at','updated_at']
+
     list_per_page = 5
 
 # admin.site.register(Presentation)
-admin.site.register(Snippet)
+# admin.site.register(Snippet)
--- mainsite/biportal/models.py
+++ mainsite/biportal/models.py
@@ -43,6 +43,7 @@ class Presentation(models.Model):
         )
 
     created_at = models.DateTimeField(
+        editable=False,
         auto_now=True,
         # default=timezone.now()
         )
@@ -62,6 +63,8 @@ class Presentation(models.Model):
         return self.name
 
     class Meta:
+        verbose_name = 'Presentation'
+        verbose_name_plural = 'Presentations'
         ordering = ["-created_at"]
 
 ###########################################################################################################
@@ -82,18 +85,83 @@ class Bipage(models.Model):
 
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name = 'BI Page'
+        verbose_name_plural = 'BI Pages'
 
 ###########################################################################################################
 class Snippet(models.Model):
-    name = models.CharField(max_length=50, blank=True)
-    file = models.ImageField()
-    description = models.CharField(max_length=255, blank=True)
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(
+        max_length=50,
+        blank=True
+        )
+
+    # embedded = models.TextField(
+    embedded = models.URLField(
+        max_length=2000,
+        blank=True
+        )
+
+    # file will be uploaded to MEDIA_ROOT/image_rendered
+    image_rendered = models.ImageField(
+        upload_to='image_rendered/',
+        verbose_name='Rendered Report',
+        null = True
+        )
+
+    image_cropped = models.ImageField(
+        upload_to='image_cropped/',
+        verbose_name='Report Snippet',
+        null = True
+        )
+
+    x = models.PositiveIntegerField(
+        default=0
+        )
+
+    y = models.PositiveIntegerField(
+        default=0
+        )
+
+    w = models.PositiveIntegerField(
+        default=0
+        )
+
+    h = models.PositiveIntegerField(
+        default=0
+        )
+
+    created_at = models.DateTimeField(
+        editable=False,
+        auto_now=True,
+        )
+
+    updated_at = models.DateTimeField(
+        auto_now_add=True,
+        )
+
+    created_by = models.ForeignKey(
+        User,
+        related_name='snippet_creator',
+        null = True,
+        on_delete=models.CASCADE
+        )
+
+    updated_by = models.ForeignKey(
+        User,
+        related_name='snippet_updater',
+        null = True,
+        on_delete=models.CASCADE
+        )
+
+    # will be available as bipage.pages_set and snippet.bipages
+    pages = models.ManyToManyField(Bipage)
 
     tags = TaggableManager()
 
     def __str__(self):
         return self.name
+
     class Meta:
         verbose_name = 'snippet'
         verbose_name_plural = 'snippets'
